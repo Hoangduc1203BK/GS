96f5e14dd55d4da4a233b15eb585b3704fbeb134
DucNguyen19 <nguyenduc059912@gmail.com>
2023-07-24 22:55:31 +0700

    update ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			boxShadow: {
+				price: "0 4px 4px rgba(0, 0, 0, 0.3)",
+				beautiful: "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+				shadowGood: "rgba(50, 50, 93, 0.25) 0px 10px 20px -5px, rgba(0, 0, 0, 0.3) 0px 5px 10px -8px",
+				buttonHome: "0px 10px 30px 0px rgba(0, 67, 144, 0.3)",
+				businessHome: "0px 10px 40px 10px rgba(47, 128, 237, 0.1)",
+				hoverBusiness: "0 8px 16px 0 rgba(98,168,253,.4)",
+				hoverButtonPrice: "0px 4px 4px 0px #00000040;",
+			},
 		},
 		fontFamily: {
 			custom: [
